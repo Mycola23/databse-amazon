e60fa27fe7e38c487398f4d2da12bfe0a01856a8
person <litvinenkomycola263@ukr.net>
2024-07-14 15:58:35 +0300

    maybe last update of all server

--- js/script.js
+++ js/script.js
@@ -7,12 +7,9 @@ const corsOptions = {
     origin: "*",
 };
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    res.append("Access-Control-Allow-Origin", ["*"]);
-    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.append("Access-Control-Allow-Headers", "Content-Type");
-    next();
-}); // we can do iton our pet project but on real better be no repeat this because we`ll have great problems with control
+app.use(express.json());
+
+// we can do iton our pet project but on real better be no repeat this because we`ll have great problems with control
 
 const products = [
     {
